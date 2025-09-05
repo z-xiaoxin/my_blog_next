7bb41688627adb69be189859a55000b349d4bacf
xiaoxin <2471685565@qq.com>
2025-09-05 11:30:52 +0800

    feat: 修改docker compose

--- deploy/local_start.sh
+++ deploy/local_start.sh
@@ -5,4 +5,4 @@ DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build
 docker save first-next-app:latest mongo:7.0 -o blog-images.tar
 gzip blog-images.tar
 
-scp -i ~/.ssh/id_ed25519_aliyun blog-images.tar.gz Chris@8.148.204.76:/home/Chris/codehub/my_blog/blog_docker
+scp -i ~/.ssh/id_ed25519_aliyun blog-images.tar.gz Chris@8.148.204.76:/home/Chris/codehub/my_blog/my_blog_next
